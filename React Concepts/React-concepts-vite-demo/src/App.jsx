29abfbbfc5882b5aa8ffa824a47be7513bd19255
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-04-07 06:25:48 +0000

    create new demo project

---
+++ React Concepts/React-concepts-vite-demo/src/App.jsx
@@ -0,0 +1,7 @@
+import Button from './Components/Button.jsx';
+
+function app(){
+  return (<Button />);
+}
+
+export default app;
---
+++ React Concepts/React-concepts-vite-demo/src/Components/Button.jsx
@@ -0,0 +1,7 @@
+function Button(){
+
+    const handleClick = () => {console.log("ouch!");}
+    return (<button onClick= {handleClick}>Click me dawg </button>);
+}
+
+export default Button
